b2cc1b5f9feb1fff8988da7f0a177a3092e1a5b9
Saikiran Pulijala <SaiPulijala@ad.m-ize.com>
2019-08-09 13:17:56 +0530

    updating script

--- RemoveExcessInstance.sh
+++ RemoveExcessInstance.sh
@@ -1,74 +1,71 @@
 #!/bin/bash
 
-aws ecs list-container-instances --cluster $1 > containerinstances.json
-
-jq -r '.containerInstanceArns[]' containerinstances.json > containerinstances.txt
-
-sed -i -r 's/.*\///' containerinstances.txt
-
-
-count=0
-
-while read -r i
+for cluster in ccplay-sandbox-standard ccplay-sandbox-large ccplay-sandbox-mlarge
 do
-	aws ecs describe-container-instances --cluster $1 --container-instances $i > containerinfo.json
-	if [[ $(jq -r '.containerInstances[].runningTasksCount' containerinfo.json) == 0 ]]
-	then
-		InstanceID=$(jq -r '.containerInstances[].ec2InstanceId' containerinfo.json)
-		
-		echo $i 
-		echo $InstanceID
-		echo $i,$InstanceID >> containerinstancestoremove.txt
-		count=$((count+1))
-		echo $count
-	fi
-done < containerinstances.txt
+	if [[ $cluster == "ccplay-sandbox-standard" ]]; then ASG=ccplay-sandbox-standard-autoscalling-group; fi
+	if [[ $cluster == "ccplay-sandbox-large" ]]; then ASG=ccplay-sandbox-large-autoscalling-group; fi
+	if [[ $cluster == "ccplay-sandbox-mlarge" ]]; then ASG=ccplay-sandbox-mlarge-autoscalling-group; fi
+
+	echo $cluster
+	echo $ASG
+
+	aws ecs list-container-instances --cluster $cluster > containerinstances.json
+
+	jq -r '.containerInstanceArns[]' containerinstances.json > containerinstances.txt
+
+	sed -i -r 's/.*\///' containerinstances.txt
 
 
+	count=0
 
-if [[ $count == 0 ]] || [[ $count == 1 ]]
-then
-	echo $count
-	rm -fr containerinstancestoremove.txt
-	exit
-else
-	echo "container instances with 0 Running tasks are $count"
-	count=$((count-1))
-	echo "container instances with 0 running to be removed $count"
-fi
-
-
-while IFS=, read -r a b 
-do
-	
- 	
-		echo $a
-		echo $b
-		echo $count
-		
-		read -p "Draining $a, Detaching $b and terminating $b [y/n] :" value < /dev/tty
-		
-		if [[ $value == "y" ]]
+	while read -r i
+	do
+		aws ecs describe-container-instances --cluster $cluster --container-instances $i > containerinfo.json
+		if [[ $(jq -r '.containerInstances[].runningTasksCount' containerinfo.json) == 0 ]]
 		then
+			InstanceID=$(jq -r '.containerInstances[].ec2InstanceId' containerinfo.json)
 		
-		aws ecs update-container-instances-state --cluster $1 --container-instances $a --status DRAINING
+			echo $i 
+			echo $InstanceID
+			echo $i,$InstanceID >> containerinstancestoremove.txt
+			count=$((count+1))
+			echo $count
+		fi
+	done < containerinstances.txt
+
+
+
+	while IFS=, read -r a b 
+	do
+	
+ 
+		echo $count
+			
+		if [[ $count != 0 ]] && [[ $count != 1 ]]
+		then 
 		
-		aws autoscaling detach-instances --instance-ids $b --auto-scaling-group-name $2 --should-decrement-desired-capacity
+		echo $a
+		
+		echo $b
+		
+		aws ecs update-container-instances-state --cluster $cluster --container-instances $a --status DRAINING
+		
+		aws autoscaling detach-instances --instance-ids $b --auto-scaling-group-name $ASG --should-decrement-desired-capacity
 		
 		aws ec2 terminate-instances --instance-ids $b
 		
 		fi
 		
 		count=$((count-1))
+		
 		echo $count
-		if [[ $count == 0 ]]
-                then
-				rm -fr containerinstancestoremove.txt
-                        exit
-                fi
+		
+	
+	done < containerinstancestoremove.txt
+	
+	
 
-
-	fi
-done < containerinstancestoremove.txt
-
-
+	rm -f containerinstancestoremove.txt
+	
+	
+done
